f5f0eb27d19c7e985deed20427538dbf19424119
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-05-10 10:34:42 -0400

    fix: Rename theme

--- patterns/page-breadcrumb.php
+++ patterns/page-breadcrumb.php
@@ -2,7 +2,7 @@
 
 /**
  * Title: page breadcrumb
- * Slug: open-spedning-coalition/page-breadcrumb
+ * Slug: www.open-spending.eu/page-breadcrumb
  * Categories: page
  * Keywords: page breadcrumb
  * Block Types: core/template-part/page-breadcrumb
